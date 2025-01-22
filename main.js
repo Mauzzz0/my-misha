ebb3c0d418e2709414f93e1740982be68eaf1715
Ruslan Semak <ruslan.semack1501@gmail.com>
2025-01-22 14:45:30 +0300

    Задание по теме циклы

--- main.js
+++ main.js
@@ -1,12 +1,4 @@
-const arr = [1, 2, 3];
-
-const newValue = 'test';
-
-arr[0] = newValue;
-
-arr[arr.length - 1] = newValue;
-
-const middleIndex = Math.floor(arr.length / 2);
-arr[middleIndex] = newValue;
-
-console.log(arr);
+// Задание по теме "Циклы"
+for (let i = 0; i < 3; i++) {
+  console.log(i);
+}
