3f2ba9a594f3ae46dd48842a36d009802be52aca
Ruslan Semak <ruslan.semack1501@gmail.com>
2025-02-04 18:14:32 +0300

    Просто пример коммита из UI WebStorm

--- main.js
+++ main.js
@@ -2,16 +2,3 @@
 for (let i = 0; i < 3; i++) {
   console.log(i);
 }
-
-// Задание по теме "#1"
-console.log(1);
-
-// Задание по теме "#2"
-console.log(2);
-
-// Задание по теме "#3"
-console.log(3);
-
-// Задание по теме "#4"
-console.log(4);
-
